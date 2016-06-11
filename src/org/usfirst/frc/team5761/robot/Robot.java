77527c48b752678192f5bda1acb9569fd40d14d8
Jiah Pang <jiah@pang.net.au>
2016-06-11 12:20:58 +1000

    Adding custom drive function
    This has the proper Logitech arcade joystick

--- src/org/usfirst/frc/team5761/robot/Robot.java
+++ src/org/usfirst/frc/team5761/robot/Robot.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team5761.robot;
 
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,8 +21,10 @@ public class Robot extends IterativeRobot {
 
     static final Logger LOG = LoggerFactory.getLogger(Robot.class);
 
-    RobotDrive myRobot;
+    //RobotDrive myRobot;
     Joystick stick;
+    Spark leftMC;
+    Spark rightMC;
     int autoLoopCounter;
 
     /**
@@ -33,8 +37,11 @@ public class Robot extends IterativeRobot {
         // load the DriverStation SmartDashboard
         new DriverStationSmartDashboard();  // ToDo: perhaps this should be static
 
-        myRobot = new RobotDrive(0, 1);
+        //myRobot = new RobotDrive(0, 1);
+        leftMC = new Spark(0);
+        rightMC = new Spark(1);
         stick = new Joystick(0);
+
         LOG.info("robotInit: END");
     }
 
@@ -51,10 +58,10 @@ public class Robot extends IterativeRobot {
     public void autonomousPeriodic() {
         if (autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
         {
-            myRobot.drive(-0.5, 0.0);    // drive forwards half speed
+            //myRobot.drive(-0.5, 0.0);    // drive forwards half speed
             autoLoopCounter++;
         } else {
-            myRobot.drive(0.0, 0.0);    // stop robot
+            //myRobot.drive(0.0, 0.0);    // stop robot
         }
     }
 
@@ -72,9 +79,61 @@ public class Robot extends IterativeRobot {
      */
     public void teleopPeriodic() {
 
-        myRobot.arcadeDrive(stick);
+        //myRobot.arcadeDrive(stick);
+        double side = stick.getX();
+        double speed = stick.getY();
+        double throttle = stick.getThrottle();
+        //LOG.debug("teleopPeriodic: raw [side:" + side + "][speed:" + speed + "][throttle:" + throttle + "]");
+
+        throttle = (throttle - 1)/2;
+        speed = speed * throttle;
+        side = side * throttle;
+
+        double appliedSpeed = speed * throttle;
+
+        LOG.debug("teleopPeriodic: [side:" + side + "][speed:" + speed + "][throttle:" + throttle + "]");
+        SmartDashboard.putString("side", ""+side);
+        SmartDashboard.putString("speed", ""+speed);
+        SmartDashboard.putString("throttle", ""+throttle);
+
+
+
+        double leftMotorPower = -speed + -side ;
+        double rightMotorPower = speed + -side;
+
+
+        drive(leftMotorPower, rightMotorPower);
+
+        //This is a TEST - WILL REMOVE UNLESS THIS IS THE BEST OPTION
+       // if (button() )
     }
 
+    /**
+     * This method will drive the motors of the robot based on the inputs.
+     *
+     * @param leftPower
+     * @param rightPower
+     */
+    private void drive(double leftPower, double rightPower)
+    {
+        LOG.debug("drive: [leftPower:" + leftPower + "][rightPower:" + rightPower + "]");
+
+        SmartDashboard.putString("leftPower", ""+leftPower);
+        SmartDashboard.putString("rightPower", ""+rightPower);
+
+        leftMC.set(leftPower);
+        rightMC.set(rightPower);
+
+    }
+
+    private void stop()
+    {
+        leftMC.set(0);
+        rightMC.set(0);
+
+        LOG.debug("Stop");
+
+    }
     /**
      * This function is called periodically during test mode
      */
